0f2582ab94aae05f596dd862127503c54b620045
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-15 16:49:13 +0000

    Added a guide which is output before the game starts. Implemented the feedback for the user's guess

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -34,6 +34,9 @@ int main() {
     std::string answer = words[random(0,99)];
     std::vector<char> answerVector(answer.begin(), answer.end());
     std::cout << "Welcome to Legally Distinct Wordle! You have " << maxAttempts << " attempts to guess the 5 letter word.\n";
+    std::cout << "If a letter is cyan, like this: (" << "\033[1;36m" << "a" << "\033[0m" << "), that means the letter is correct and in the right position.\n";
+    std::cout << "If a letter is yellow, like this: (" << "\033[1;33m" << "b" << "\033[0m" << "), that means the letter is in the answer but it's in the wrong position.\n";
+    std::cout << "If a letter has no special colour, like this: (c), that means it is not in the answer";
     std::cout << answer << '\n';
     while (attempts < maxAttempts && !correct){
         std::cout << "Enter a word: ";
@@ -47,5 +50,17 @@ int main() {
             std::cerr << "Invalid input!\n";
             continue;
         }
+        for (size_t counter = 0; counter < guess.length(); counter++) {
+            if (guess [counter] == answer [counter]) {
+                std::cout << "\033[1;36m" << guess[counter] << "\033[0m\n";
+            }
+            else if (answer.find(guess[counter]) != std::string::npos) {
+                std::cout << "\033[1;33m" << guess[counter] << "\033[0m\n";
+            }
+            else {
+                std::cout << guess[counter];
+            }
+        }
     }
+    std::cout << '\n';
 }
