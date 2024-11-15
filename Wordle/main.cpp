1fa7c752023a4610d594f1de5fc466c22772c6bb
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-15 11:06:59 +0000

    Initial Commit

---
+++ Wordle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Wordle
+//
+//  Created by Sam Hart on 15/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
