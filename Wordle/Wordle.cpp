35bc1a76435bd50e47d6358572b1d7053a325138
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-18 21:49:07 +0000

    Added code to prevent extra appearances of a letter in the user's guess from being marked as correct but in the wrong position. They are now marked the same as incorrect letters, like in original Wordle.

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -4,7 +4,6 @@
 
 #include "Checks.hpp"
 #include "Clearer.hpp"
-
 #include "Randomiser.hpp"
 
 int main() {
@@ -39,11 +38,15 @@ int main() {
         std::string guess;
         std::string answer = words[random(0,99)];
         std::vector<char> answerVector(answer.begin(), answer.end());
+        int answerCounter[26] = {0};
+            for (char character : answer) {
+                answerCounter[character - 'a']++;
+            }
         
         std::cout << "Welcome to Legally Distinct Wordle! You have " << maxAttempts << " attempts to guess the 5 letter word.\n";
         std::cout << "If a letter is cyan, like this: (" << "\033[1;36m" << "a" << "\033[0m" << "), that means the letter is correct and in the right position.\n";
         std::cout << "If a letter is yellow, like this: (" << "\033[1;33m" << "b" << "\033[0m" << "), that means the letter is in the answer but it's in the wrong position.\n";
-        std::cout << "If a letter has no special colour, like this: (c), that means it is not in the answer\n";
+        std::cout << "If a letter has no special colour, like this: (c), that means you've guessed the character more times than it appears in the answer (if the letter appears in your guess more than once and it shows as blue or yellow earlier) or it doesn't appear in the answer at all.\n";
         std::cout << "Psst, for debugging purposes, the answer is " << answer << ".\n";
         
         while (attempts < maxAttempts && !correct){
@@ -59,15 +62,26 @@ int main() {
                 continue;
             }
             
+            int guessCounter[26] = {0};
+            
             std::transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
             std::vector<char> guessVector(guess.begin(), guess.end());
             
             for (size_t counter = 0; counter < guess.length(); counter++) {
+                int characterIndex = guess[counter] - 'a';
+                
                 if (guess [counter] == answer [counter]) {
                     std::cout << "\033[1;36m" << guess[counter] << "\033[0m";
+                    guessCounter[characterIndex]++;
                 }
                 else if (answer.find(guess[counter]) != std::string::npos) {
-                    std::cout << "\033[1;33m" << guess[counter] << "\033[0m";
+                    if (guessCounter[characterIndex] < answerCounter[characterIndex]) {
+                        std::cout << "\033[1;33m" << guess[counter] << "\033[0m";
+                        guessCounter[characterIndex]++;
+                    }
+                    else {
+                        std::cout << guess[counter];
+                    }
                 }
                 else {
                     std::cout << guess[counter];
